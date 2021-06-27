9370ba7b56369e384694bbf90cd26f931611d301
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2021-06-27 01:00:33 -0600

    Clase CasillaBonoDados creados

--- EscalerasYSerpientes/src/Casilla/CasillaBonoDados.java
+++ EscalerasYSerpientes/src/Casilla/CasillaBonoDados.java
@@ -12,29 +12,23 @@ import java.util.List;
  *
  * @author baquiax
  */
-public class CasillaPierdeTurno extends Casilla implements CasillaEspecial {
+public class CasillaBonoDados extends Casilla implements CasillaEspecial {
 
-    public CasillaPierdeTurno(int fila, int columna, int id) {
+    public CasillaBonoDados(int fila, int columna, int id) {
         super(fila, columna, id);
     }
 
-    public CasillaPierdeTurno(int fila, int columna) {
+    public CasillaBonoDados(int fila, int columna) {
         super(fila, columna);
     }
 
-    public CasillaPierdeTurno(int id) {
+    public CasillaBonoDados(int id) {
         super(id);
     }
 
-    /**
-     * Hace que el jugador pierda el turno para el siguiente turno
-     *
-     * @param jugador
-     * @param casillas
-     */
     @Override
     public void accionCasilla(Jugador jugador, List<Casilla> casillas) {
-        jugador.setConTurno(false);
+        System.out.println("Ganaste otro turno");
     }
 
 }
